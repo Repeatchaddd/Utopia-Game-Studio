3f36a52991067575f8bfe57697da20969fab9988
Garin Shaw <161399829+Repeatchaddd@users.noreply.github.com>
2026-09-13 09:26:25 -0500

    v0.95.02 execute graph-driven Blueprint movement on Wii U

--- runtime_template/source/main.c
+++ runtime_template/source/main.c
@@ -6,13 +6,6 @@
 #include "renderer.h"
 #include "room_data.h"
 
-static int bp_speed_percent(int idx, int fallback, int lo, int hi) {
-    int value = idx >= 0 ? bp_vars[idx] : fallback;
-    if (value < lo) value = lo;
-    if (value > hi) value = hi;
-    return value;
-}
-
 static int room_blocked(int x, int y) {
     const int points[4][2] = {{x,y},{x+PLAYER_W-1,y},{x,y+PLAYER_H-1},{x+PLAYER_W-1,y+PLAYER_H-1}};
     for (int i=0;i<4;++i) {
@@ -50,50 +43,31 @@ int main(int argc, char **argv)
     unsigned int animation_frame = 0;
     int facing = 0; /* down, left, right, up */
     const UtopiaAnimation *previous_animation = 0;
+    int bp_first_frame = 1;
     while (WHBProcIsRunning()) {
-        int move_x = 0, move_y = 0, running = 0;
-        int speed_x = PLAYER_SPEED, speed_y = PLAYER_SPEED;
+        int move_x100 = 0, move_y100 = 0, run_percent = 100;
+        uint32_t hold = 0, trigger = 0;
         VPADRead(VPAD_CHAN_0, &input, 1, &error);
         if (error == VPAD_READ_SUCCESS) {
-            BPApplyTriggered(input.trigger);
-#if BP_MOVE_LEFT
-            if ((input.hold & VPAD_BUTTON_LEFT) && bp_gate(BP_MOVE_LEFT_GATE_VAR, BP_MOVE_LEFT_GATE_OP, BP_MOVE_LEFT_GATE_VALUE)) {
-                move_x -= 1; speed_x = PLAYER_SPEED * bp_speed_percent(BP_MOVE_LEFT_SPEED_VAR, BP_MOVE_LEFT_SPEED, 1, 400) / 100;
-            }
-#endif
-#if BP_MOVE_RIGHT
-            if ((input.hold & VPAD_BUTTON_RIGHT) && bp_gate(BP_MOVE_RIGHT_GATE_VAR, BP_MOVE_RIGHT_GATE_OP, BP_MOVE_RIGHT_GATE_VALUE)) {
-                move_x += 1; speed_x = PLAYER_SPEED * bp_speed_percent(BP_MOVE_RIGHT_SPEED_VAR, BP_MOVE_RIGHT_SPEED, 1, 400) / 100;
-            }
-#endif
-#if BP_MOVE_UP
-            if ((input.hold & VPAD_BUTTON_UP) && bp_gate(BP_MOVE_UP_GATE_VAR, BP_MOVE_UP_GATE_OP, BP_MOVE_UP_GATE_VALUE)) {
-                move_y -= 1; speed_y = PLAYER_SPEED * bp_speed_percent(BP_MOVE_UP_SPEED_VAR, BP_MOVE_UP_SPEED, 1, 400) / 100;
-            }
-#endif
-#if BP_MOVE_DOWN
-            if ((input.hold & VPAD_BUTTON_DOWN) && bp_gate(BP_MOVE_DOWN_GATE_VAR, BP_MOVE_DOWN_GATE_OP, BP_MOVE_DOWN_GATE_VALUE)) {
-                move_y += 1; speed_y = PLAYER_SPEED * bp_speed_percent(BP_MOVE_DOWN_SPEED_VAR, BP_MOVE_DOWN_SPEED, 1, 400) / 100;
-            }
-#endif
-#if BP_RUN_ENABLED
-            if ((input.hold & BP_RUN_BUTTON) && bp_gate(BP_RUN_GATE_VAR, BP_RUN_GATE_OP, BP_RUN_GATE_VALUE)) running = 1;
-#endif
+            hold = input.hold; trigger = input.trigger;
+            BPApplyFrameActions(trigger, bp_first_frame);
+            BPCollectMovement(hold, bp_first_frame, &move_x100, &move_y100, &run_percent);
             if (input.trigger & VPAD_BUTTON_PLUS) break;
         }
+        bp_first_frame = 0;
+        int delta_x = PLAYER_SPEED * move_x100 / 100;
+        int delta_y = PLAYER_SPEED * move_y100 / 100;
+        if (run_percent != 100) { delta_x = delta_x * run_percent / 100; delta_y = delta_y * run_percent / 100; }
         /* 181/256 is approximately 1/sqrt(2), keeping diagonals from moving faster. */
-        if (running) { int run_percent = bp_speed_percent(BP_RUN_SPEED_VAR, BP_RUN_SPEED, 101, 400); speed_x = speed_x * run_percent / 100; speed_y = speed_y * run_percent / 100; }
-        if (move_x && move_y) { speed_x = speed_x * 181 / 256; speed_y = speed_y * 181 / 256; }
-        if (speed_x < 1) speed_x = 1;
-        if (speed_y < 1) speed_y = 1;
-        int next_x = x + move_x * speed_x;
-        int next_y = y + move_y * speed_y;
+        if (delta_x && delta_y) { delta_x = delta_x * 181 / 256; delta_y = delta_y * 181 / 256; }
+        int next_x = x + delta_x;
+        int next_y = y + delta_y;
         if (!room_blocked(next_x, y)) x = next_x;
         if (!room_blocked(x, next_y)) y = next_y;
-        if (move_y > 0) facing = 0;
-        else if (move_x < 0) facing = 1;
-        else if (move_x > 0) facing = 2;
-        else if (move_y < 0) facing = 3;
+        if (delta_y > 0) facing = 0;
+        else if (delta_x < 0) facing = 1;
+        else if (delta_x > 0) facing = 2;
+        else if (delta_y < 0) facing = 3;
         if (x < 0) x = 0;
         if (y < 0) y = 0;
         if (x > 1280 - PLAYER_W) x = 1280 - PLAYER_W;
@@ -104,8 +78,8 @@ int main(int argc, char **argv)
 #if HAS_ANIMATION
         const UtopiaAnimation *idle[] = {&ANIM_IDLE_DOWN, &ANIM_IDLE_LEFT, &ANIM_IDLE_RIGHT, &ANIM_IDLE_UP};
         const UtopiaAnimation *walk[] = {&ANIM_WALK_DOWN, &ANIM_WALK_LEFT, &ANIM_WALK_RIGHT, &ANIM_WALK_UP};
-        const UtopiaAnimation *animation = (move_x || move_y) ? walk[facing] : idle[facing];
-        if (!animation->count) animation = (move_x || move_y) ? idle[facing] : walk[facing];
+        const UtopiaAnimation *animation = (delta_x || delta_y) ? walk[facing] : idle[facing];
+        if (!animation->count) animation = (delta_x || delta_y) ? idle[facing] : walk[facing];
         if (animation != previous_animation) { animation_frame = 0; animation_tick = 0; previous_animation = animation; }
         if (animation->count) {
             UtopiaRendererDrawSprite(x, y, PLAYER_W, PLAYER_H, animation->frames[animation_frame], animation->width, animation->height);
