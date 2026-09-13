cb11fac613cb9e9b05854c7ac7ecee29be42b551
Garin Shaw <161399829+Repeatchaddd@users.noreply.github.com>
2026-09-13 08:56:33 -0500

    Add default v0.9 room data header

---
+++ runtime_template/source/room_data.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+#define ROOM_COLS 40
+#define ROOM_ROWS 23
+#define TILE_SIZE 32
+#define TILE_COUNT 0
+static const uint32_t *const ROOM_TILES[1] = {0};
+static const uint8_t ROOM_SOLID[1] = {0};
+static const uint8_t ROOM_MAP[ROOM_COLS*ROOM_ROWS] = { [0 ... (ROOM_COLS*ROOM_ROWS-1)] = 255 };
