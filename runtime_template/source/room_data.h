278d24702a64c72a91869a02ff1362e4119caf3c
Garin Shaw <161399829+Repeatchaddd@users.noreply.github.com>
2026-09-13 08:56:57 -0500

    Simplify default room data stub

--- runtime_template/source/room_data.h
+++ runtime_template/source/room_data.h
@@ -6,4 +6,4 @@
 #define TILE_COUNT 0
 static const uint32_t *const ROOM_TILES[1] = {0};
 static const uint8_t ROOM_SOLID[1] = {0};
-static const uint8_t ROOM_MAP[ROOM_COLS*ROOM_ROWS] = { [0 ... (ROOM_COLS*ROOM_ROWS-1)] = 255 };
+static const uint8_t ROOM_MAP[ROOM_COLS*ROOM_ROWS] = {0};
